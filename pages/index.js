2cc9de9bdafbcb7208375b1c3902865895b3679e
rakesh T <rakesh.tagadghar@dltapps.co.uk>
2021-04-27 23:08:17 +0530

    covid app

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,108 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useEffect, useState } from "react";
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import { Table, Tag, Space } from "antd";
+import "antd/dist/antd.css";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+am4core.useTheme(am4themes_animated);
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+function App(props) {
+  const [covid, setCovid] = useState(null);
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+  useEffect(() => {
+    fetch("https://api.covid19india.org/data.json")
+      .then((res) => res.json())
+      .then((x) => setCovid(x));
+  }, []);
+  console.log(covid);
+  if (process.browser) {
+    // Create chart instance
+    var chart = am4core.create("chartdiv", am4charts.XYChart);
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+    // Add data
+    chart.data = covid?.statewise;
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+    // Create axes
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+    var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = "state";
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.minGridDistance = 30;
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+    categoryAxis.renderer.labels.template.adapter.add("dy", function (dy) {
+      return dy;
+    });
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+    // Create series
+    var series = chart.series.push(new am4charts.ColumnSeries());
+    series.dataFields.valueY = "active";
+    series.dataFields.categoryX = "state";
+    series.name = "active";
+    series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+    series.columns.template.fillOpacity = 0.8;
+
+    var columnTemplate = series.columns.template;
+    columnTemplate.strokeWidth = 2;
+    columnTemplate.strokeOpacity = 1;
+
+    const columns = [
+      {
+        title: "Name",
+        dataIndex: "state",
+        key: "state",
+        render: (text) => text,
+        sorter: (a, b) => a.state.length - b.state.length,
+      },
+      {
+        title: "Active",
+        dataIndex: "active",
+        key: "active",
+        render: (text) => text,
+        sorter: {
+          compare: (a, b) => a.active - b.active,
+        },
+      },
+      {
+        title: "Confirmed",
+        dataIndex: "confirmed",
+        key: "confirmed",
+        render: (text) => text,
+        sorter: {
+          compare: (a, b) => a.active - b.active,
+        },
+      },
+      {
+        title: "Deaths",
+        dataIndex: "deaths",
+        key: "deaths",
+        render: (text) => text,
+        sorter: {
+          compare: (a, b) => a.active - b.active,
+        },
+      },
+      {
+        title: "Recovered",
+        dataIndex: "recovered",
+        key: "recovered",
+        render: (text) => text,
+        sorter: {
+          compare: (a, b) => a.active - b.active,
+        },
+      },
+    ];
+
+    return (
+      <>
+        <h1 className="text-center">Active Covid Cases India</h1>
+        <Table columns={columns} dataSource={covid?.statewise} />
+        <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
+      </>
+    );
+  }
+  return null;
 }
+
+export default App;
